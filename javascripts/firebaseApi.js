4e0f352f599360e60767f9e0b6d76ce313c430b6
Caitlin Murnick <caitlinmurnick@gmail.com>
2017-10-24 17:20:32 -0500

    copied zoe updates

--- javascripts/firebaseApi.js
+++ javascripts/firebaseApi.js
@@ -1,3 +1,4 @@
+
 "use strict";
 
 let firebaseKey = "";
@@ -9,17 +10,36 @@ const setKey = (key) =>{
 
 //Firebase: GOOGLE - Use input credentials to authenticate user.
 let authenticateGoogle = () => {
-return new Promise((resolve, reject) => {
-  var provider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth().signInWithPopup(provider)
-    .then((authData) => {
-    	userUid = authData.user.uid;
-        resolve(authData.user);
-    }).catch((error) => {
-        reject(error);
-    });
-});
+	return new Promise((resolve, reject) => {
+	  var provider = new firebase.auth.GoogleAuthProvider();
+	  firebase.auth().signInWithPopup(provider)
+	    .then((authData) => {
+	    	userUid = authData.user.uid;
+	        resolve(authData.user);
+	    }).catch((error) => {
+	        reject(error);
+	    });
+	});
+};
+
+const getMovieList = () => {
+	let movies = [];
+	return new Promise((resolve, reject) =>{
+		$.ajax(`${firebaseKey.databaseURL}/movies.json?orderBy="uid"&equalTo="${userUid}"`).then((fbMovies) =>{
+			if(fbMovies != null){
+				Object.keys(fbMovies).forEach((key) =>{
+					fbMovies[key].id = key;
+					movies.push(fbMovies[key]);
+				});
+			}
+
+			resolve(movies);
+		}).catch((err) =>{
+			reject(err);
+		});
+	});
 };
 
 
-module.exports = {setKey, authenticateGoogle};
+
+module.exports = {setKey, authenticateGoogle, getMovieList};
