c00f23c8199342d5305af7f701c24df872990e0d
Caitlin Murnick <caitlinmurnick@gmail.com>
2017-10-23 21:00:24 -0500

    authenticated

--- dist/app.js
+++ dist/app.js
@@ -67,6 +67,7 @@ module.exports = {domString, clearDom};
 "use strict";
 
 const tmdb = require('./tmdb');
+const firebaseApi = require('./firebaseApi');
 
 const pressEnter = () => {
   $(document).keypress((e) => {
@@ -97,18 +98,47 @@ const myLinks = () => {
 	});
 };
 
-module.exports = {pressEnter, myLinks};
-},{"./tmdb":6}],4:[function(require,module,exports){
+const googleAuth = () => {
+	$("#googleButton").click((e) => {
+		firebaseApi.authenticateGoogle().then ((result) => {
+			console.log("result", result);
+		}).catch((err) => {
+			console.log("error in authenticateGoogle", err);
+		});
+	});
+};
+
+
+
+
+
+module.exports = {pressEnter, myLinks, googleAuth};
+},{"./firebaseApi":4,"./tmdb":6}],4:[function(require,module,exports){
 "use strict";
 
 let firebaseKey = "";
-
+let userUid = "";
 
 const setKey = (key) =>{
 	firebaseKey = key;
 };
 
-module.exports = {setKey};
+//Firebase: GOOGLE - Use input credentials to authenticate user.
+let authenticateGoogle = () => {
+return new Promise((resolve, reject) => {
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider)
+    .then((authData) => {
+    	userUid = authData.user.uid;
+        resolve(authData.user);
+    }).catch((error) => {
+        reject(error);
+    });
+});
+};
+
+
+module.exports = {setKey, authenticateGoogle};
 },{}],5:[function(require,module,exports){
 "use strict";
 
@@ -118,9 +148,10 @@ let events = require('./events');
 let apiKeys = require('./apiKeys');
 
 apiKeys.retrieveKeys();
-
-events.pressEnter();
 events.myLinks();
+events.googleAuth();
+events.pressEnter();
+
 
 },{"./apiKeys":1,"./dom":2,"./events":3}],6:[function(require,module,exports){
 "use strict";
